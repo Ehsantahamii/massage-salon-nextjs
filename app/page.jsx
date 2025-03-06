2f4a2d5ba3a73791047e449c983f9c0e0f57cc8c
sajjad <sajjadhasanikakh06@gmail.com>
2025-03-06 12:54:32 +0330

    #sajad added about me

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
+import About from "@/components/template/about/About";
 import Hero from "@/components/template/hero/Hero";
 
 export default function Home() {
   return (
     <main className="">
       <Hero/>
+      <About/>
     </main>
   );
 }
