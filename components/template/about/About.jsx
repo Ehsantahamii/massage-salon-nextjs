02ae1b9e4214cfea84a55cda391db6ba32ec495a
ehsan <ehsantahamii@gmail.com>
2025-03-11 14:18:14 +0330

    commit #8 ehsan

--- components/template/about/About.jsx
+++ components/template/about/About.jsx
@@ -20,23 +20,23 @@ const About = () => {
     <div className="w-full my-14 max-md:mt-[-150px] ">
       <div className="about flex flex-col lg:flex-row lg:justify-between justify-center items-center gap-8 ">
         <div className="img_about w-[80%] mx-auto lg:w-[40%] relative">
-          <svg className="absolute z-[-1]" id="sw-js-blob-svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">                    <defs>                         <linearGradient id="sw-gradient" x1="0" x2="1" y1="1" y2="0">                            <stop id="stop1" stop-color="rgba(57, 122, 192, 1)" offset="0%"></stop>                            <stop id="stop2" stop-color="rgba(20.404, 66.885, 116.941, 1)" offset="100%"></stop>                        </linearGradient>                    </defs>                <path fill="url(#sw-gradient)" d="M33.5,-19.3C39.3,-9.2,37.1,5.5,30.2,16C23.3,26.5,11.7,32.8,0.5,32.5C-10.6,32.2,-21.3,25.3,-27.3,15.3C-33.4,5.3,-34.9,-7.9,-29.6,-17.7C-24.3,-27.4,-12.1,-33.8,0.8,-34.3C13.8,-34.8,27.6,-29.4,33.5,-19.3Z" width="100%" height="100%" transform="translate(50 50)" stroke-width="0" stroke="url(#sw-gradient)"></path>              </svg>
+          <svg className="absolute w-[400px] right-[12rem] lg:right-auto z-[-1]" id="sw-js-blob-svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">                    <defs>                         <linearGradient id="sw-gradient" x1="0" x2="1" y1="1" y2="0">                            <stop id="stop1" stop-color="rgba(57, 122, 192, 1)" offset="0%"></stop>                            <stop id="stop2" stop-color="rgba(20.404, 66.885, 116.941, 1)" offset="100%"></stop>                        </linearGradient>                    </defs>                <path fill="url(#sw-gradient)" d="M33.5,-19.3C39.3,-9.2,37.1,5.5,30.2,16C23.3,26.5,11.7,32.8,0.5,32.5C-10.6,32.2,-21.3,25.3,-27.3,15.3C-33.4,5.3,-34.9,-7.9,-29.6,-17.7C-24.3,-27.4,-12.1,-33.8,0.8,-34.3C13.8,-34.8,27.6,-29.4,33.5,-19.3Z" width="100%" height="100%" transform="translate(50 50)" stroke-width="0" stroke="url(#sw-gradient)"></path>              </svg>
           <Image
             src="/img/maryam-khalili.png"
             width={2524}
             height={4000}
             style={{ borderRadius: "9rem 0.5rem 9rem 0.5rem" }}
             alt="مریم خلیلی زاده"
-            className=" rounded-xl z-[10] drop-shadow"
+            className=" rounded-xl mx-auto lg:mx-0 max-w-[420px] z-[10] drop-shadow"
           />
         </div>
 
         <div className="content_about flex flex-col leading-10 w-[95%] mx-auto lg:w-[60%]">
           <h3 className="text-[50px] font-bold mb-10">درباره من</h3>
-          <h1 className="text-[30px] font-bold">مریم خلیلی زاده </h1>
+          <h2 className="text-[30px] font-bold">مریم خلیلی زاده </h2>
           <p className="font-bold">کارشناسی تربیت بدنی </p>
           <p className="font-bold text-justify max-md:text-[15px]">
-            “من به عنوان یک ماساژور حرفه ای با بیش از 4 سال تجربه در زمینه ماساژ
+            “من به عنوان یک ماسور حرفه ای با بیش از 4 سال تجربه در زمینه ماساژ
             درمانی و ریلکسیشن، همواره تلاش کرده‌ام تا با استفاده از تکنیک‌های
             مدرن و سنتی، به بهبود سلامت جسمی و روحی مراجعین خود کمک کنم. من به
             دقت به نیازهای هر فرد توجه می‌کنم و برنامه‌های ماساژ شخصی‌سازی شده
@@ -45,7 +45,7 @@ const About = () => {
           <div className="title_skill text-[30px] font-bold mt-5">مهارت ها</div>
           <div className="skilss flex justify-around items-center flex-wrap mt-2 gap-1">
             {skills.map((index) => (
-              <div key={index.id} className="bg-gray-200 flex justify-around items-center flex-wrap mt-2 py-1 px-2 rounded-md"><h1>{index.text}</h1></div>
+              <div key={index.id} className="bg-gray-200 flex justify-around items-center flex-wrap mt-2 py-1 px-2 rounded-md"><p>{index.text}</p></div>
             ))}
           </div>
 
