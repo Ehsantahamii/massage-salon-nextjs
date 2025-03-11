5ea54b9ec57ecde6040c2341320c7bcd53c1fb47
ehsan <ehsantahamii@gmail.com>
2025-03-11 23:20:05 +0330

    commit #10 ehsan

--- components/module/movingtext/MovingText.jsx
+++ components/module/movingtext/MovingText.jsx
@@ -10,10 +10,10 @@ const MovingText = () => {
             scrollTrigger: {
                 trigger: '.moving-text',
                 start: 'top top',
-                end: '+=50%',
+                end: '+=20%',
                 scrub: true,
-                smooth: 2,
-                speed: 6,
+                smooth: 10,
+                speed: 1,
                 effects: true,
                 pin: true,
             },
@@ -24,7 +24,7 @@ const MovingText = () => {
             src="/img/MASSAGE.svg"
             width={1280}
             height={204}
-            className="moving-text w-[85%] mx-auto mt-[-80px] max-md:mt-[-400px]"
+            className="moving-text w-[90vw] lg:w-[85vw] mx-auto mt-[-6.5rem]"
         />);
 };
 
--- components/template/about/About.jsx
+++ components/template/about/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </div>
         </div>
       </div>
-    </main>
+    </div>
   );
 };
 
