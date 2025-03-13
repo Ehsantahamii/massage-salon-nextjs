1d3c90181625d13a3830d5181325f9f359525461
sajjad <sajjadhasanikakh06@gmail.com>
2025-03-13 11:30:18 +0330

    #sajjad blog edit

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -9,18 +9,24 @@ const Blog = () => {
           <h1> هرآنچه باید بدانید!</h1>
           <div className="flex items-center gap-1">
             <span className="text-[15px]">وبلاگ/</span>
-            <Link href="/" className="text-[15px] text-[#D8BF91] z-10">صفحه اصلی</Link>
+            <Link href="/" className="text-[15px] text-[#D8BF91] z-10">
+              صفحه اصلی
+            </Link>
           </div>
         </div>
         <div className="title_home flex items-center  w-[1000px] max-lg:w-[550px] max-md:w-[250px] mt-10 mx-auto">
-            <h3 className="text-[25px] font-bold">دسته بندی</h3>
-            <img src="/img/Vectors_blog.svg" className="w-[30px]" />
+          <h3 className="text-[25px] font-bold">دسته بندی</h3>
+          <img src="/img/Vectors_blog.svg" className="w-[30px]" />
         </div>
-        <div className="content_blog bg-red-500 w-[1000px]  mx-auto mt-8 mb-8 max-lg:w-[550px] max-md:w-[250px]">
-          <Itemsblog/>
+        <div className=" content_blog w-[1000px]  mx-auto mt-8 mb-8 max-lg:w-[550px] max-md:w-[250px]">
+          <Itemsblog />
         </div>
       </main>
-      <img src="/img/Vector_blog.svg" alt="" className="w-[1528px] mt-[-17rem] max-md:mt-[-5rem]" />
+      <img
+        src="/img/Vector_blog.svg"
+        alt=""
+        className="w-[1528px] mt-[-95rem] max-lg:mt-[-65rem] max-md:hidden"
+      />
     </div>
   );
 };
